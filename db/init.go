048b59d1d9c732a80d717652c589a8e93b4b837f
Mayank mohan <mayankmohan@Dotpes-MacBook-Air-2.local>
2023-05-06 13:50:00 +0530

    database-initialised

--- db/init.go
+++ db/init.go
@@ -1,5 +1,33 @@
 package db
 
-func Init() error {
+import (
+	"go-message_queue_system/domain"
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var Client *sql.DB
+
+func Init(dbCred domain.DBCred) error {
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s", dbCred.Username, dbCred.Password, dbCred.Hostname, dbCred.DBName))
+	if err != nil {
+		log.Printf("Error %s when opening DB\n", err)
+		return err
+	}
+	db.SetMaxOpenConns(20)
+	db.SetMaxIdleConns(20)
+	db.SetConnMaxLifetime(time.Minute * 5)
+
+	pingErr := db.Ping()
+    if pingErr != nil {
+        log.Printf("Error: %v, unable to ping database", pingErr)
+		return err
+    }
+	log.Printf("Connected to DB %s successfully\n", dbCred.DBName)
+	Client = db
 	return nil
 }
--- main.go
+++ main.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"go-message_queue_system/bootstrap"
 	"go-message_queue_system/db"
+	"go-message_queue_system/domain"
 	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := db.Init()
+	err := godotenv.Load()
 	if err != nil {
-		log.Printf("Error: %v, unable to init DB", err)
+		log.Printf("Error: %v, unable to load .env file", err)
+		return
+	}
+
+	dbCred := getCredentials()
+	err = db.Init(dbCred)
+	if err != nil {
+		log.Printf("Error: %v, unable to connect to database", err.Error())
+		return
+	}
+	if err != nil {
+		log.Printf("Error: %v, unable to init DB", err.Error())
 		return
 	}
 
@@ -19,5 +34,19 @@ func main() {
 	apiGroup := r.Group("/api")
 	bootstrap.Init(apiGroup)
 
-	r.Run(":8000")
+	PORT := os.Getenv("PORT")
+	err = r.Run(fmt.Sprintf(":%v", PORT))
+	if err != nil {
+		log.Printf("Error: %v, unable to run server", err.Error())
+	}
+	log.Printf("Server Running on PORT: %v", PORT)
+}
+
+func getCredentials() domain.DBCred {
+	return domain.DBCred{
+		Username: os.Getenv("username"),
+		Password: os.Getenv("password"),
+		Hostname: os.Getenv("hostname"),
+		DBName:   os.Getenv("dbname"),
+	}
 }
