5a1dce99f8354b614ff403b0caecb74c45d1afcf
Mayank mohan <mayankmohan@Dotpes-MacBook-Air-2.local>
2023-05-07 16:27:18 +0530

    download and save image implementation

--- pkg/catalog/controller/helper.go
+++ pkg/catalog/controller/helper.go
@@ -1,7 +1,55 @@
 package controller
 
-import "context"
+import (
+	"context"
+	"errors"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+)
 
 func downloadAndSaveImage(ctx context.Context, imagesArr []string) ([]string, error) {
-	return make([]string, 0), nil
+	localImagesPathArr := make([]string, 0)
+	
+	outputDir := "./images"
+	err := os.MkdirAll(outputDir, 0755)
+	if err != nil {
+		log.Printf("Error: %v,\n failed_to_make_directory", err.Error())
+		return nil, errors.New("unable to create directory")
+	}
+
+	for _, imageUrl := range imagesArr {
+		fileName := filepath.Base(imageUrl)
+		outputPath := filepath.Join(outputDir, fileName)
+
+		err := createFile(imageUrl, outputPath)
+		if err != nil {
+			log.Printf("Error: %v,\n failed_to_create_local_file", err.Error())
+			return nil, errors.New("unable to create local file")
+		}
+		localImagesPathArr = append(localImagesPathArr, outputPath)
+	}
+	return localImagesPathArr, nil
+}
+
+func createFile(originalUrl, outputPath string) error {
+	file, err := os.Create(outputPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	response, err := http.Get(originalUrl)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	_, err = io.Copy(file, response.Body)
+	if err != nil {
+		return err
+	}
+	return nil
 }
